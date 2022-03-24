742caee0a621dc94ebbc14bd2fbd540763b1bade
HiagoRaianEH <hiagoraian@estudiohaus.com>
2022-03-24 17:29:34 -0300

    validate login insomnia

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL:"http//localhost:5000"
+});
+
+export const createSession = async (email,password)=>{
+    return api.post("/sessions",{email,password});
+}
